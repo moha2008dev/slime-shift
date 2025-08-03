a5a879e667d6560e8c55cd460905f5601678fa1f
moha2008dev <moha2008meddz@gmail.com>
2025-08-03 23:28:34 +0200

    some eddits

--- scripts/credits.gd
+++ scripts/credits.gd
@@ -5,11 +5,11 @@ var previos_scene = "res://scenes/main_menu.tscn"
 func _ready():
 	$credits_up.play("up")
 
-func _on_TextureButton_pressed():
-	get_tree().change_scene(previos_scene)
-
-
 
 func _on_credits_up_animation_finished(anim_name):
 	if anim_name == "up":
 		get_tree().change_scene(previos_scene)
+
+
+func _on_exitb_pressed():
+	get_tree().change_scene(previos_scene)
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,23 +2,23 @@ extends Control
 
 
 
-func _on_new_pressed():
+func _on_new2_pressed():
 	var loading_scene = preload("res://scenes/loading.tscn")
 	get_tree().change_scene_to(loading_scene)
 
 
-func _on_load_pressed():
+func _on_load2_pressed():
 	var loading_scene = preload("res://scenes/loading.tscn")
 	get_tree().change_scene_to(loading_scene)
 
-func _on_options_pressed():
+func _on_options2_pressed():
 	get_tree().change_scene("res://scenes/test.tscn")
 
 
-func _on_credits_pressed():
+func _on_credits2_pressed():
 	get_tree().change_scene("res://scenes/credits.tscn")
 
 
-func _on_exit_pressed():
+func _on_exit2_pressed():
 	get_tree().quit()
 
