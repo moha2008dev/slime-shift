eb473e7f3d91c43965ec8bfdc81f1afaab42c59b
moha2008dev <moha2008meddz@gmail.com>
2025-08-06 23:59:44 +0200

    adding transform platform

--- scripts/StateArea.gd
+++ scripts/StateArea.gd
@@ -5,3 +5,4 @@ export(String,"Normal","Iced","Liquid") var state : String = "Normal"
 func _on_StateArea_body_entered(body : Node2D):
 	if body.is_in_group("Player"):
 		body.state = state
+	$"../transformer/AnimatedSprite2".play(state)
